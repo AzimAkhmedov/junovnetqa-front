30e8ef161611f1f7ae4515186598f82411ff86d7
CECK <97340544+AzimAkhmedov@users.noreply.github.com>
2023-08-08 00:38:16 +0500

    init: routes, components, pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { About, Help, Home } from "./pages";
+import { Header } from "./components";
 
 function App() {
   return (
     <div className="App">
-  
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/help" element={<Help />} />
+      </Routes>
     </div>
   );
 }
