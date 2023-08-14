98c82fca86d67dfccaa70de5b465cb602f823ef5
CECK <97340544+AzimAkhmedov@users.noreply.github.com>
2023-08-14 15:36:42 +0500

    init: scss setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import { Routes, Route } from "react-router-dom";
 import { About, Help, Home } from "./pages";
 import { Header } from "./components";
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
-
+import React from "react";
+import { Box } from "@mui/material";
+import s from "./Header.module.scss";
+import { NavLink } from "react-router-dom";
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <Box className={"container " + s.headerContainer}>
+        <ul>
+          <li>
+            {" "}
+            <NavLink
+              to={"/"}
+              className={({ isActive }) => (isActive ? s.active : "")}
+            >
+              логотип
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/about"}
+              className={({ isActive }) => (isActive ? s.active : "")}
+            >
+              о нас
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/help"}
+              className={({ isActive }) => (isActive ? s.active : "")}
+            >
+              поддержка
+            </NavLink>
+          </li>
+        </ul>
+      </Box>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
