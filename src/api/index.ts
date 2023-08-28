76eb259b2318ffbbaf489fee63fa2e891a24e05e
CECK <97340544+AzimAkhmedov@users.noreply.github.com>
2023-08-28 17:19:47 +0500

    feat: api

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,6 +3,15 @@ import instance from "./instance"
 const api = {
     getAllJobs() {
         return instance.get("api/v1/jobs/").then(res => res)
+    },
+    getHrJobs() {
+        return instance.get("api/v1/hr-questions/").then(res => res)
+    },
+    getTechJobs(job_id: number | string, grade_id: number | string) {
+        return instance.get(`/api/v1/tech-questions/${job_id}/${grade_id}/`).then(res => res)
+    },
+    getGrades() {
+        return instance.get("api/v1/grades/").then(res => res)
     }
 }
 
